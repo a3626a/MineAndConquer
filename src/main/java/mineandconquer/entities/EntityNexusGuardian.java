caaf75c317d57c2eae9500da654dbba86d953bfc
a3626a <a3626a@naver.com>
2014-06-27 02:54:54 +0900

    넥서스 가디언을 계속 손보고 있다.
    이제 완벽하게 움직이지 못하는 병신이 되었다고 자부하다. 체력은 200이다.

--- src/main/java/mineandconquer/entities/EntityNexusGuardian.java
+++ src/main/java/mineandconquer/entities/EntityNexusGuardian.java
@@ -1,42 +1,20 @@
 package mineandconquer.entities;
 
-import java.util.Calendar;
-
-import net.minecraft.block.Block;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.EnumCreatureAttribute;
-import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIArrowAttack;
-import net.minecraft.entity.ai.EntityAIAttackOnCollide;
-import net.minecraft.entity.ai.EntityAIFleeSun;
-import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAIRestrictSun;
-import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.stats.AchievementList;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldProviderHell;
 
 public class EntityNexusGuardian extends EntityMob implements IRangedAttackMob {
 
@@ -46,8 +24,8 @@ public class EntityNexusGuardian extends EntityMob implements IRangedAttackMob {
 		this.tasks.addTask(6, new EntityAIWatchClosest(this,
 				EntityPlayer.class, 8.0F));
 		this.tasks.addTask(6, new EntityAILookIdle(this));
-		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this,
-				EntityPlayer.class, 0, true));
+		//this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this,
+		//		EntityPlayer.class, 0, true));
 		this.tasks.addTask(4,
 				new EntityAIArrowAttack(this, 1.0D, 20, 60, 15.0F));
 		// TODO Auto-generated constructor stub
@@ -58,7 +36,7 @@ public class EntityNexusGuardian extends EntityMob implements IRangedAttackMob {
 		// TODO Auto-generated method stub
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(200);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0D);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(5);
 	}
 	
 	@Override
@@ -100,37 +78,24 @@ public class EntityNexusGuardian extends EntityMob implements IRangedAttackMob {
 	}
 
 	@Override
-	protected boolean isMovementCeased() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
 	protected boolean isAIEnabled() {
 		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
-	public boolean canBePushed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	protected void collideWithEntity(Entity par1Entity) {
-		// TODO Auto-generated method stub
-	}
-	
-	@Override
 	public void knockBack(Entity par1Entity, float par2, double par3,
 			double par5) {
 		// TODO Auto-generated method stub
+		return;
 	}
+	// 빡치게 넉백은 addVelocity를 쓰지 않고 직접적으로 속도를 변화시킨다.
 	
 	@Override
-	public void onUpdate() {
+	public void addVelocity(double par1, double par3, double par5) {
 		// TODO Auto-generated method stub
-		super.onUpdate();
+		return;
 	}
+	// 속도를 더하는 것을 막음으로써 위치 변화를 막는다
+	
 }
