e268f574d1b9f2c2ee606d8e166600025fdb7dcb
a3626a <a3626a@naver.com>
2014-07-01 19:04:29 +0900

    플레이어 리스폰 제어 시도중
    잘안되네

--- src/main/java/mineandconquer/core/handler/ForgeEventHandler.java
+++ src/main/java/mineandconquer/core/handler/ForgeEventHandler.java
@@ -3,10 +3,19 @@ package mineandconquer.core.handler;
 import mineandconquer.MineAndConquer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler {
-
+	
+	@SubscribeEvent
+	public void EventRespawn(LivingDeathEvent event) {
+		if (event.entity instanceof EntityPlayer) {
+			System.out.println("Respawn");
+			event.setCanceled(true);
+		}
+	}
+	
 	@SubscribeEvent
 	public void EventPVP(LivingAttackEvent event) {
 		if (event.entity instanceof EntityPlayer
