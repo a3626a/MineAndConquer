9560c470f39421895eb7bc5b911b6174be8fcc74
ShinHeeChan <shc812@gmail.com>
2014-07-13 11:14:10 +0900

    테스트용

--- src/main/java/mineandconquer/inventory/SlotGrinder.java
+++ src/main/java/mineandconquer/inventory/SlotGrinder.java
@@ -1,6 +1,10 @@
 package mineandconquer.inventory;
 
+import mineandconquer.blocks.ModBlocks;
 import mineandconquer.items.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -13,7 +17,8 @@ public class SlotGrinder extends Slot{
 	//only place item(only accept)
 	@Override
 	public boolean isItemValid(ItemStack par1IS){
-		return true;
+		ItemStack is = new ItemStack(Blocks.cobblestone);
+		return par1IS.getItem() == is.getItem();
 	}
 }
 
