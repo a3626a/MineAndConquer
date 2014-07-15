909d0a3c9cdbab8310ea5ea727c05ae9c250023c
ShinHeeChan <shc812@gmail.com>
2014-07-15 16:37:39 +0900

    레서피 등록을 못한다.... ㅂㄷㅂㄷㅂㄷㅂㄷ
    ㅂㄷㅂㄷㅂㄷㅂㄷㅂㄷㅂㄷㅂㄷㅂㄷ

--- src/main/java/mineandconquer/lib/GrinderRecipes.java
+++ src/main/java/mineandconquer/lib/GrinderRecipes.java
@@ -1,8 +1,37 @@
 package mineandconquer.lib;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
 public class GrinderRecipes {
 	public static final GrinderRecipes grindingBase = new GrinderRecipes();
+	public static final int cobblestoneID = ItemID(Item.getItemFromBlock(Blocks.cobblestone));
+	public static final int gravelID = ItemID(Item.getItemFromBlock(Blocks.gravel));
+	public static final int sandID = ItemID(Item.getItemFromBlock(Blocks.sand));
+	public static final int ironoreID = ItemID(Item.getItemFromBlock(Blocks.iron_ore));
+	public static final int obsidianID = ItemID(Item.getItemFromBlock(Blocks.obsidian));
+	public static final int diamondID = ItemID(Items.diamond);
+	public static final int ironID = ItemID(Items.iron_ingot);
 	public static GrinderRecipes grinding(){
 		return grindingBase;
+	}
+	
+	private static int ItemID(Item item){
+		return Item.getIdFromItem(item);
+	}
+	
+	public Item getGrindResult(HashSet<Integer> set){
+		HashSet<Integer> set1 = new HashSet<Integer>();
+		set1.add(cobblestoneID);
+		set1.add(gravelID);
+		if(set.equals(set1)){
+			return Item.getItemFromBlock(Blocks.sand);
+		}
+		return Items.diamond;
 	}
 }
